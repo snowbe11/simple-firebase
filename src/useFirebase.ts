b631a4eedfa6401bd5d84d288c4f6075fb1114a8
snowpapa@home <snowpapa@gmail.com>
2022-07-14 19:56:41 +0900

    패키지 설치 후 사용하지 못하는 오류 수정
    파일 소스 경로와 참조 수정

---
+++ src/useFirebase.ts
@@ -0,0 +1,41 @@
+import {
+  addDocument,
+  deleteDocument,
+  FirebaseValueObject,
+  getCollection,
+  getDocument,
+} from ".";
+import React, { useCallback } from "react";
+
+export const useFirebase = <T extends FirebaseValueObject>(
+  collection: string
+) => {
+  const list = useCallback(async () => {
+    return await getCollection(collection);
+  }, [collection]);
+
+  const get = useCallback(
+    async (name: string) => {
+      return await getDocument<T>(collection, name);
+    },
+    [collection]
+  );
+
+  const add = useCallback(
+    async (name: string, values: T) => {
+      return await addDocument<T>(collection, name, values);
+    },
+    [collection]
+  );
+
+  const remove = useCallback(
+    async (name: string) => {
+      return await deleteDocument(collection, name);
+    },
+    [collection]
+  );
+
+  return { list, get, add, remove };
+};
+
+export default useFirebase;
